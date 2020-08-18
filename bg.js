ac7f14f536ac053f6f9e0581d1c46be9f7946f29
five1star <[five1star0420@gmail.com]>
2020-08-18 21:51:13 +0900

    add bg.js

---
+++ bg.js
@@ -0,0 +1,36 @@
+const body = document.querySelector('body');
+
+const IMG_NUMBER = 5;
+
+
+function handleImgLoaded(){
+    console.log('finish loaded')
+}
+
+
+function paintImage(imgNumber){
+    const image = new Image();
+    image.src = `images/${imgNumber + 1}.jpg`;
+    image.classList.add('bgImage');
+    body.appendChild(image);
+}
+
+
+function genRandom(){
+    const number = Math.floor(Math.random()*IMG_NUMBER);
+
+    return number;
+
+
+}
+// Math.random();
+// 
+function init(){
+    const randomNumber = genRandom();
+    paintImage(randomNumber)
+
+
+}
+
+
+init();
